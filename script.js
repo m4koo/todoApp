1134b7ee3b9a976806d2d537655f3b88ba0d9577
m4koo <mahmut.kalem@outlook.de>
2022-11-30 10:30:12 +0100

    shortend changeButton func
    changeEditButton and changeStopButton --> changeButton

--- script.js
+++ script.js
@@ -57,21 +57,13 @@ function edit(i) {
     const title = parent.querySelector('h2');
     const note = parent.querySelector('span');
     
-    changeEditButton(i);
+    changeButton(i, '#f24b4b', 'Stop', 'stopEdit'); //changes button to stop
     if(title){
         title.contentEditable = "true";
     }
     note.contentEditable = "true";
 
     noteFocus(note);
-}
-
-
-function changeEditButton(i) {
-    let button = document.getElementById(`btn${i}`);
-    button.style.backgroundColor = '#f24b4b'
-    button.innerHTML="Stop";
-    button.setAttribute("onclick", `stopEdit(${i})`);
 }
 
 
@@ -86,20 +78,22 @@ function stopEdit(i) {
     const parent = document.querySelector(`#item${i}`);
     const title = parent.querySelector('h2');
     const note = parent.querySelector('span');
-
-    changeStopButton(i)
+    
+    // changeStopButton(i)
+    changeButton(i, 'var(--teal)', 'Edit',  'edit') //changes button to edit
     if (title){
         title.contentEditable = "false";
     }
     note.contentEditable = "false";
 }
 
-function changeStopButton(i) {
+
+function changeButton(i, color, btnText, btnFunc){ // switch between edit and stop
     let button = document.getElementById(`btn${i}`);
-    button.style.backgroundColor = 'var(--teal)';
-    button.innerHTML="Edit";
-    button.setAttribute("onclick", `edit(${i})`);
-}
+    button.style.backgroundColor = `${color}`;
+    button.innerHTML = `${btnText}`
+    button.setAttribute("onclick",  `${btnFunc}(${i})`)
+} 
 
 
 // LOCAL STORAGE 
@@ -107,7 +101,7 @@ function changeStopButton(i) {
 function save(){
     let localTitles = JSON.stringify(titles);
     let localNotes = JSON.stringify(notes);
-
+    
     localStorage.setItem('titles', localTitles);
     localStorage.setItem('notes', localNotes);
 }
@@ -121,3 +115,19 @@ function load(){
         notes = JSON.parse(getNotes);
     }
 }
+
+
+
+// function changeStopButton(i) {
+//     let button = document.getElementById(`btn${i}`);
+//     button.style.backgroundColor = 'var(--teal)';
+//     button.innerHTML="Edit";
+//     button.setAttribute("onclick", `edit(${i})`);
+// }
+
+// function changeEditButton(i) {
+//     let button = document.getElementById(`btn${i}`);
+//     button.style.backgroundColor = '#f24b4b'
+//     button.innerHTML="Stop";
+//     button.setAttribute("onclick", `stopEdit(${i})`);
+// }
