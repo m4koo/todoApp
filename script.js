14f7a93a94c56979c91ab06e25b71bc845efb47a
m4koo <mahmut.kalem@outlook.de>
2022-11-30 01:00:19 +0100

    CRUD
    you can create, read, update and delete Notes.
    + saves in Local Storage

---
+++ script.js
@@ -0,0 +1,123 @@
+let titles = [];
+let notes = [];
+load();
+
+function render(){
+    list.innerHTML='';
+    loadNotes();
+}
+
+
+function addNote(){
+    let title = document.getElementById('title');
+    let note = document.getElementById('note');
+
+    if(note.value <= 0 && title.value <= 0){
+        return;
+    }else{
+        titles.push(title.value);
+        notes.push(note.value);
+    
+        note.value = '';
+        title.value = '';
+    }
+
+    render();
+    save();
+}
+
+
+function loadNotes(){
+    document.getElementById('list');
+    for(let i = 0; i < notes.length; i++){
+        list.innerHTML +=`
+        <div class="container" id="item${i}">
+            ${titles[i]==''?'':'<h2 contenteditable="false" spellcheck="false">' + titles[i] + '</h2>'}
+            <span contenteditable="false" spellcheck="false">${notes[i]}</span>
+            <div class="buttons">
+                <button onclick="edit(${i})" id="btn${i}">Edit</button>
+                <button onclick="remove(${i})">X</button>
+            </div>
+        </div>
+        `
+    }
+}
+
+
+function remove(i) {
+    titles.splice(i, 1);
+    notes.splice(i, 1);
+    render();
+    save();
+}
+
+
+function edit(i) {
+    const parent = document.querySelector(`#item${i}`);
+    const title = parent.querySelector('h2');
+    const note = parent.querySelector('span');
+    
+    changeEditButton(i);
+    if(title){
+        title.contentEditable = "true";
+    }
+    note.contentEditable = "true";
+
+    noteFocus(note);
+}
+
+
+function changeEditButton(i) {
+    let button = document.getElementById(`btn${i}`);
+    button.style.backgroundColor = '#f24b4b'
+    button.innerHTML="Stop";
+    button.setAttribute("onclick", `stopEdit(${i})`);
+}
+
+
+function noteFocus(note) {
+    note.focus();
+    window.getSelection().selectAllChildren(note);
+    window.getSelection().collapseToEnd(); //fokus am ende 
+}
+
+
+function stopEdit(i) {
+    const parent = document.querySelector(`#item${i}`);
+    const title = parent.querySelector('h2');
+    const note = parent.querySelector('span');
+
+    changeStopButton(i)
+    if (title){
+        title.contentEditable = "false";
+    }
+    note.contentEditable = "false";
+}
+
+function changeStopButton(i) {
+    let button = document.getElementById(`btn${i}`);
+    button.style.backgroundColor = 'var(--teal)';
+    button.innerHTML="Edit";
+    button.setAttribute("onclick", `edit(${i})`);
+}
+
+
+// LOCAL STORAGE 
+
+function save(){
+    let localTitles = JSON.stringify(titles);
+    let localNotes = JSON.stringify(notes);
+
+    localStorage.setItem('titles', localTitles);
+    localStorage.setItem('notes', localNotes);
+}
+
+
+function load(){
+    let getTitles = localStorage.getItem('titles');
+    let getNotes = localStorage.getItem('notes');
+    if (getTitles && getNotes){
+        titles = JSON.parse(getTitles);
+        notes = JSON.parse(getNotes);
+    }
+}
